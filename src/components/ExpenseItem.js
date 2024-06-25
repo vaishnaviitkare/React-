6aa3617b3062a79ac0b9d3c184b71d24bd07afa6
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-25 15:04:23 +0530

    creating first custom component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return (
+        <div>
+       <p>Food - $5</p> 
+        <p>Petrol - $20</p>
+        <p>Movie - $10</p> 
+        </div>
+    );
+}
+//we can export this component so that we can import it wherever we need
+export default ExpenseItem;
