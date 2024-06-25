307636eb61c607cc4419bee0274962866387c541
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-25 17:13:00 +0530

    update file with location in it and one comment

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,8 +1,10 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
+    //we wrap of many div or items in one div because js can return one item at a time
     return (
       <div className="expense-item">
         <div>15 August 2023</div>
+        <div className="expense-item__location">Delhi</div>
         <div className="expense-item__description">
           <h2>Book</h2>
           <div className="expense-item__price">$10</div>
