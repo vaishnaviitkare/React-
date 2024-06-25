b17beeaa2e2c9057b6c1f64b1fd9c8b95cdb9238
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-25 18:27:12 +0530

    Use javascript inside jsx

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,17 @@
 import "./ExpenseItem.css";
 function ExpenseItem() {
     //we wrap of many div or items in one div because js can return one item at a time
+    const expenseDate=new Date(2024,6,25).toISOString();
+    const expenseTitle="Insurance";
+    const expenseAmount=50;
+    const expenseLocation="Bangalore";
     return (
       <div className="expense-item">
-        <div>15 August 2023</div>
-        <div className="expense-item__location">Delhi</div>
+        <div>{expenseDate}</div>
+        <div className="expense-item__location">{expenseLocation}</div>
         <div className="expense-item__description">
-          <h2>Book</h2>
-          <div className="expense-item__price">$10</div>
+          <h2>{expenseTitle}</h2>
+          <div className="expense-item__price">${expenseAmount}</div>
         </div>
       </div>
     );
