24e38ca1ab04463b9f807b31877ffbc973025224
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-26 17:00:40 +0530

    In App.js we store all values in the array of object and then we iterate them and then call component nothing change in ExpenseItem.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import ExpenseItem from "./components/ExpenseItem"
 function App() {
+  const expenses=[
+    { id: "1", date: new Date(2023, 7, 15), location:"Bangalore", title: "Insurance", price: 100 },
+    { id: "2", date: new Date(2023, 3, 25), location:"Delhi", title: "Book", price: 10 },
+    { id: "3", date: new Date(2023, 10, 11), location:"Hyderabad", title: "Pen", price: 1 },
+    { id: "4", date: new Date(2023, 1, 14), location:"Mumbai", title: "Laptop", price: 200 },
+ 
+  ]
   return (
     <div>
-         <ExpenseItem 
-         date={new Date(2023, 7, 15)} 
-         location="Bangalore" 
-         title="Insurance" 
-         price="50">
-         </ExpenseItem>
-         <ExpenseItem
-         date={new Date(2023, 3, 25)}
-        location="Delhi"
-        title="Book"
-        price="20"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 10, 11)}
-        location="Hyderabad"
-        title="Pen"
-        price="5"
-      ></ExpenseItem>
-      <ExpenseItem
-        date={new Date(2023, 1, 14)}
-        location="Mumbai"
-        title="Laptop"
-        price="200"
-        >
-         </ExpenseItem>
+      {expenses.map((expense,index)=>{
+          return(
+            <ExpenseItem
+            key={expense.id}
+            date={expense.date}
+            location={expense.location}
+            title={expense.title}
+            price={expense.price}
+            >
+            </ExpenseItem>
+          )
+      })}
     </div>
   );
 }
