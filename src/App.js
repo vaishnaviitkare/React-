1737e7b267e3c779d5571013ed940f2e498b328d
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-26 18:20:41 +0530

    we format the date and give it calender look as month year and date

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import ExpenseItem from "./components/ExpenseItem"
 function App() {
   const expenses=[
-    { id: "1", date: new Date(2023, 7, 15), location:"Bangalore", title: "Insurance", price: 100 },
+    { id: "1", date: new Date(2023, 7, 2), location:"Bangalore", title: "Insurance", price: 100 },
     { id: "2", date: new Date(2023, 3, 25), location:"Delhi", title: "Book", price: 10 },
     { id: "3", date: new Date(2023, 10, 11), location:"Hyderabad", title: "Pen", price: 1 },
     { id: "4", date: new Date(2023, 1, 14), location:"Mumbai", title: "Laptop", price: 200 },
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,24 @@
 import "./ExpenseItem.css";
+const months=["January","February","March","April","May","June","July","August","September","October","November","December"];
 function ExpenseItem(props) {
     //we wrap of many div or items in one div because js can return one item at a time
-    return (
-      <div className="expense-item">
-        <div>{props.date.toISOString()}</div>
+    const month=months[props.date.getMonth()];
+    const year=props.date.getFullYear();
+    const date=props.date.getDate()<10?'0'+props.date.getDate():props.date.getDate();
+    return(
+        <div className="expense-item">
+        <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{date}</div>
+        </div>
         <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
           <h2>{props.title}</h2>
           <div className="expense-item__price">${props.price}</div>
         </div>
       </div>
-    );
+      );
   }
 //we can export this component so that we can import it wherever we need
 export default ExpenseItem; 
