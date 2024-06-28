ff09f8eaf3d5881e1240e31c8003d0e060d59e84
vaishnaviitkare <vaishuitkare@gmail.com>
2024-06-28 13:27:34 +0530

    add event listner to button and after click change the title and this change should be displayed on screen using hook useState

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,10 +1,11 @@
-import React from 'react';
+import React,{useState} from 'react';
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItem=(props)=> {
+  const [title,setTitle]=useState(props.title);
   function buttonClick(){
-    console.log("Button clicked");
+    setTitle("New Title");
   }
     //we wrap of many div or items in one div because js can return one item at a time
     return(
@@ -12,7 +13,7 @@ const ExpenseItem=(props)=> {
           <ExpenseDate date={props.date}></ExpenseDate>
         <div className="expense-item__location">{props.location}</div>
         <div className="expense-item__description">
-          <h2>{props.title}</h2>
+          <h2>{title}</h2>
           <div className="expense-item__price">${props.price}</div>
         </div>
         <button onClick={buttonClick}>Clicked</button>
