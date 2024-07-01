46be85556f19031f5237be38a60278846bc7f3ac
vaishnaviitkare <vaishuitkare@gmail.com>
2024-07-01 17:36:41 +0530

    we do previous commit text as - do all logic in Newexpense and just add cancel event using props in expenseform it is easy to read

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -4,7 +4,6 @@ const ExpenseForm=(props)=>{
     const [enteredTitle, setEnteredTitle]=useState("");
     const [enteredAmount, setEnteredAmount]=useState("");
     const [enteredDate, setEnteredDate]=useState("");
-   const [flagValue, setFlagValue]=useState(false);
    const titleChangeHandler=(event)=>{
        setEnteredTitle(event.target.value);
    }  
@@ -25,23 +24,7 @@ const ExpenseForm=(props)=>{
      setEnteredTitle("");
      setEnteredAmount("");
      setEnteredDate("");
-     setFlagValue(false);
    }
-   const formAddHandler=()=>{
-      setFlagValue(true);
-   }
-   const formCancelHandler=()=>{
-    setFlagValue(false);
-   }
-  if (flagValue===false){
-    return(
-      <form>
-      <div className="Add-expense__actions">
-        <button type="submit" onClick={formAddHandler}>Add Expense</button>
-      </div>
-      </form>
-      );
-  }
 return(
     <form onSubmit={formSubmitHandler}>
     <div className="new-expense__controls">
@@ -59,8 +42,12 @@ return(
       </div>
     </div>
     <div className="new-expense__actions">
-      <button type="button" onClick={formCancelHandler}>Cancel</button>
-      <button type="submit" onClick={formAddHandler}>Add Expense</button>
+    <button type="button"
+          onClick={() => {
+            props.onCancel();
+          }}
+        >Cancel </button>
+      <button type="submit">Add Expense</button>
     </div>
   </form>
 );
--- src/components/NewExpense/Newexpense.js
+++ src/components/NewExpense/Newexpense.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React,{useState} from "react";
 import "./NewExpense.css";
 import ExpenseForm from "./ExpenseForm";
 const NewExpense=(props)=>{
+    const [flagValue, setFlagValue]=useState(false);
     const saveExpenseDataHandler=(enteredExpenseData)=>{
        const expenseData={...enteredExpenseData,id:Math.random().toString()};
        console.log(expenseData);
        props.onSaveData(expenseData);
+       setFlagValue(false);
     }
 
 return(
 <div className="new-expense">
-<ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
+{!flagValue && (
+        <button
+          type="button"
+          onClick={() => {
+            setFlagValue(true);
+          }}
+        >
+          Add Expense
+        </button>
+      )}
+      {flagValue && (
+
+<ExpenseForm onSaveExpenseData={saveExpenseDataHandler}
+  onCancel={() => {
+   setFlagValue(false);
+  }}/>
+)}
 </div>
 );
 }
